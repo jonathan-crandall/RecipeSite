768b836328a18989090440d68350b93ffb419804
Jonathan Crandall <jonathan.u.crandall@gmail.com>
2021-08-15 18:25:16 -0600

    Created first model

--- RecipeSite/recipes/models.py
+++ RecipeSite/recipes/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
+class Recipe(models.Model):
+    food = models.CharField(max_length=100)
+    ingredients = models.CharField(max_length=4000)
+    instructions = models.CharField(max_length=4000)
+    created_on = models.DateTimeField(auto_now_add=True)
